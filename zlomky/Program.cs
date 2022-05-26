b5ad9499847256864b98569375aaf741b14bb765
FilipKohlik <4afkohlik@gmail.com>
2022-05-26 17:03:58 +0200

    first commit

---
+++ zlomky/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("napiš čitatel");
+double citatel = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("napiš jmenovatel");
+double jmenovatel = Convert.ToDouble(Console.ReadLine());
+double výsledek = citatel / jmenovatel;
+string výsledekstring = výsledek.ToString();
+int lenght = výsledekstring.Length;
+if(lenght == 18)
+{
+    Console.WriteLine("(" + výsledekstring.Substring(0, 3) + ")");
+
+
+}else
+{
+    Console.WriteLine(výsledek);
+}
